9d498b21650409eda70faa25350fa75a458fbe1d
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-01-12 18:37:40 +0530

    Password hashing done and Login endpoint added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const mongoURI = 'mongodb://localhost:27017/inotebook'
-
+mongoose.set('strictQuery', false)
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log('Connected to Mongo Successfully')
